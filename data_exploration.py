d65a47584f38a69a438018288d4c9368eef0ea78
Brian Evans <ebrian101@gmail.com>
2021-11-25 16:17:58 +0000

    Added data exploration, correlation between features

---
+++ data_exploration.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+mdf = pd.read_csv('detailed_house_sales.csv')
+pdf = pd.get_dummies(mdf, columns=['property_type'])
+
+fig, ax = plt.subplots(figsize=(8, 8))
+sns.heatmap(pdf.corr(), vmax=.8, vmin=-.8, square=True, center=0,
+            cmap=sns.diverging_palette(130, 130, as_cmap=True), ax=ax)
+plt.tight_layout()
+plt.savefig('correlation_heatmap.png')
+
+
+print('Correlation coefficients of each feature to the price')
+print(pdf.corr()['price'].abs().sort_values(ascending=False)[1:])
